9565df00fec30a4ccbf153677594f96118e75584
Artsiom <Frt@mail.ru>
2023-01-30 22:31:10 +0300

    добавлен цикл для проверки длины каждого элемента массива на соответствие условиям задачи и в случае соответствия вывод его в отдельный массив

--- Program.cs
+++ Program.cs
@@ -10,3 +10,12 @@
 Console.Write("Введите стрки: ");
 string[] str = Console.ReadLine().Split();
 PrintArray(str);
+Console.Write("[");
+for(int i = 0; i < str.Length; i++)
+{
+    if(str[i].Length <= 3)
+    {
+        Console.Write(str[i] + " ");
+    }
+}
+Console.Write("]");
