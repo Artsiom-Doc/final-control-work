854ca922ebcf58308e1cae085e7a03dfd1fbf66c
Artsiom <Frt@mail.ru>
2023-01-30 22:21:07 +0300

    добавлен метод для вывода получившегося массива строк

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿Console.Write("Введите стрки: ");
+﻿void PrintArray(string[]a)
+{
+    Console.Write("[");
+    for(int i = 0; i < a.Length; i++)
+    {
+        Console.Write(a[i]+" ");
+    }
+    Console.Write("] -> ");
+}
+Console.Write("Введите стрки: ");
 string[] str = Console.ReadLine().Split();
+PrintArray(str);
